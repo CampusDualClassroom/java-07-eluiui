f37d81851367ec5d9d82b051665a27cfe08ccbe2
Jesus Blanco Miguez <119857344+eluiui@users.noreply.github.com>
2024-09-24 23:51:11 +0200

    completado

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,9 +1,14 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
+        sumFirstNaturalNumbers(10);
+        showFirstNaturalNumbers(50);
 
     }
 
@@ -14,19 +19,47 @@ public class Exercise07 {
     // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
+        List<Integer> lista = new ArrayList<>();
+
+        for (int i = 10; i >= 1; i--) {
+            lista.add(i);
+        }
+
+        int posicion = lista.indexOf(num);
+
+        if (posicion !=-1) {
+            System.out.println("El elemento " + num + " se encuentra en la posición: " + posicion);
+        } else {
+            System.out.println("El elemento " + num + " no se encuentra en la lista.");
+        }
 
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
+        int sum = 0;
+        for (int i = num; i >= 1; i--) {
+            int numero=sum;
+            sum = i+sum;
 
+            System.out.println(i+"+"+numero+"="+sum);
+        }
+        System.out.println("Resultado "+sum);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
+        System.out.print("Los numeros positivos son: ");
+        for (int i = num; i >= 1; i--) {
+            if (i!=1){
+                System.out.print(i+", ");
+            }else {
+                System.out.print(i);
+            }
 
+        }
     }
 
 }
